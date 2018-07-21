b584b3190d4a7386d9a2b9588f26e5795088e423
Asadullah Al Hamasy <asadhamasy@gmail.com>
2018-07-22 00:48:38 +0700

    first commit

--- LoginFacebook/AppDelegate.swift
+++ LoginFacebook/AppDelegate.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -35,6 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        AppEventsLogger.activate(application)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- LoginFacebook/ViewController.swift
+++ LoginFacebook/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //var loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        var loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        
+        if let accessToken = AccessToken.current {
+            // User is logged in, use 'accessToken' here.
+            
+            print("User sudah masuk dengan token \(accessToken)")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
